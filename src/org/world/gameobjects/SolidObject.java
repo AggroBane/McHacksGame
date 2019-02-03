99f4f409e4a87d47aff369a70897928375880056
William Fecteau <william-fecteau@hotmail.com>
2019-02-03 11:48:24 -0500

    Im panicking

--- src/org/world/gameobjects/SolidObject.java
+++ src/org/world/gameobjects/SolidObject.java
@@ -1,12 +1,17 @@
 package org.world.gameobjects;
 
 
+import org.GameContainer;
+import org.graphics.Graphics;
+import org.resources.Animation;
 import org.resources.ImageResource;
+import org.world.gameobjects.tiles.Tile;
 
 public class SolidObject extends GameObject
 {
 
 	protected Hitbox hitbox;
+	protected Animation animation = null;
 	
 	public SolidObject(float x, float y, float width, float height) 
 	{
@@ -18,6 +23,29 @@ public class SolidObject extends GameObject
 	{
 		super(x, y, width, height, img);
 		hitbox = new Hitbox(x, y, width, height);
+		
+		if(img == Tile.bomb)
+		{
+			ImageResource[] bombsFrames = new ImageResource[4];
+			for(int i = 0; i < bombsFrames.length; i++)
+			{
+				bombsFrames[i] = new ImageResource("entities/bomb/bomb"+i+".png");
+			}
+			
+			animation = new Animation(bombsFrames, 5);
+		}
+	}
+	
+	public void render(Graphics g)
+	{
+		if(animation != null)
+		{
+			g.drawImage(animation.animate(), x, y, GameContainer.tileSize,  GameContainer.tileSize);
+		}
+		else
+		{
+			super.render(g);
+		}
 	}
 	
 	public Hitbox getHitbox()
